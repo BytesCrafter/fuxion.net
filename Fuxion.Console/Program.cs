57c10d7dd40546f6e66f27e5a36401214980fc12
BytesCrafter <bytescrafter@gmail.com>
2023-08-26 04:27:31 +0800

    Added a sample script for the Program.

--- Fuxion.Console/Program.cs
+++ Fuxion.Console/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        if (args.Length > 0) 
+        {
+            Program.Process(args);
+        }
+
+        else 
+        {
+            Console.WriteLine("Invalid command!");
+        }
+    }
+
+    protected static void Process(string[] strings) 
+    {
+        if( strings.Length > 0 && strings[0] == "server" ) {
+            Console.WriteLine("Server is not running...");
+        }
+
+        Console.WriteLine("Command invalid...");
+    }
+}
