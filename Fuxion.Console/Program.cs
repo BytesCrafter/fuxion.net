d6a293662765226eb3ec11ed694c94a54e2d4282
BytesCrafter <bytescrafter@gmail.com>
2023-08-25 21:19:56 +0800

    Added the implementation of the websocket server to the program with argument of 'server ipaddress port'.

--- Fuxion.Console/Program.cs
+++ Fuxion.Console/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Net;
+using System;
+
+using Fuxion.Utilities;
+using Fuxion.WebSocket;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        if (args.Length > 0) 
+        {
+            Program.Process(args);
+        }
+
+        else 
+        {
+            Console.WriteLine("Invalid command!");
+        }
+    }
+
+    protected static void Process(string[] strings) 
+    {
+        if( strings.Length > 0 && strings[0] == "server" ) {
+            Console.WriteLine("Server is not running...");
+
+            // Prepare the arguments for running the websocket server.
+            string[] commands = StringUtil.Shift(strings, 1);
+            int webport = commands.Length > 1 ? Int32.Parse(strings[0]) : 8080;
+            string webip = strings.Length > 2 ? commands[1] : "127.0.0.1";
+
+            // Instantiate WS Server and Start.
+            WebSocketServer server = new WebSocketServer();
+            server.Start(webip, webport);
+        }
+        
+        Console.WriteLine("Command invalid...");
+    }
+}
