6de51cc8c7ade6df58b7189bdffed170089fbcbe
BytesCrafter <bytescrafter@gmail.com>
2023-08-27 18:15:20 +0800

    Added demo and soon to be a tool for fuxion.

---
+++ Fuxion.Desktop/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Fuxion.Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SEND";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(26, 395);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(419, 44);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "..."});
+            this.listBox1.Location = new System.Drawing.Point(26, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(534, 355);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(578, 460);
+            this.panel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 252);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 44);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(186, 172);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(205, 48);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 463);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Fuxion Chat";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Fuxion.Desktop/Form1.cs
@@ -0,0 +1,78 @@
+﻿using Fuxion.Client;
+using Fuxion.Server;
+
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+
+namespace Fuxion.Desktop
+{
+    public partial class Form1 : Form
+    {
+        public WebSocket websock = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Connect(string ipadd, int port, string channel = "chat", string uname = "")
+        {
+            websock = new WebSocket($"ws://{ipadd}:{port}/{channel}/?name={uname}");
+
+            websock.OnMessage += Websock_OnMessage;
+            websock.OnOpen += Websock_OnOpen;
+            websock.OnClose += Websock_OnClose;
+
+            websock.Connect();
+        }
+
+        private void Websock_OnClose(object sender, CloseEventArgs e)
+        {
+            Console.WriteLine("Disconnected... ");
+            listBox1.Items.Add("\nDisconnected... ");
+        }
+
+        private void Websock_OnOpen(object sender, EventArgs e)
+        {
+            Console.WriteLine("\nConnected... ");
+            listBox1.Items.Add("\nConnected... ");
+
+            panel1.Hide();
+        }
+
+        private void Websock_OnMessage(object sender, MessageEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                Console.WriteLine("\nMessage: " + e.Data);
+                listBox1.Items.Add("\n Msg: " + e.Data);
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+                string message = textBox1.Text.ToString();
+                this.websock.Send(message);
+                textBox1.Text = "";            
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Connect("localhost", 8000, "Chat", textBox2.Text);
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            string message = textBox1.Text.ToString();
+            this.websock.Send(message);
+            textBox1.Text = "";
+        }
+    }
+}
