44d4575b3eb6e40084d1575964333370b3c01c94
BytesCrafter <bytescrafter@gmail.com>
2023-08-30 02:19:31 +0800

    Make the package.json and tsconfig.json be able to npm serve watch or build.

--- Fuxion.Node/src/index.ts
+++ Fuxion.Node/src/index.ts
@@ -1,2 +1,20 @@
 #!/usr/bin/env Fuxion
 
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: any, res: any) => {
+    res.sendFile(__dirname + '/public/demo.html');
+});
+
+io.on('connection', (socket: any) => {
+    socket.on('chat message', (msg: any) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, () => {
+    console.log(`Fuxion NET server running at http://localhost:${port}/`);
+});
